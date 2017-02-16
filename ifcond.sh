1e51601b9911f89fd45e8ac543ad688c732277d1
pathanAbdullah <abdullapathan501@gmail.com>
2017-02-16 17:17:28 +0530

    new added files

---
+++ ifcond.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#basic if information
+
+if [ $1 -gt 100 ]
+then
+echo "it print its a large no."
+pwd
+fi
+date
---
+++ ifelse.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+intA=20
+intB=30
+
+if [ intA==intB ]
+then
+echo "intA is not equal to intB"
+else
+echo "not equal"
+fi
+
+if [ -f /home/devops/traning/bash/f1 ]
+
+then
+echo "file exists"
+
+else
+
+echo "does not exicute"
+fi
